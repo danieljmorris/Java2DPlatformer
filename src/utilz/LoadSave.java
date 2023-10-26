e895d349c9832574e6c629e5303fe110d58e6913
Daniel J. Morris <dmorris3@icloud.com>
2023-10-25 20:10:43 -0400

    first commit

---
+++ src/utilz/LoadSave.java
@@ -0,0 +1,109 @@
+package utilz;
+
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Arrays;
+import java.util.List;
+
+import javax.imageio.ImageIO;
+
+public class LoadSave {
+
+	public static final String PLAYER_ATLAS = "player_sprites.png";
+	public static final String LEVEL_ATLAS = "outside_sprites.png";
+	public static final String MENU_BUTTONS = "button_atlas.png";
+	public static final String MENU_BACKGROUND = "menu_background.png";
+	public static final String PAUSE_BACKGROUND = "pause_menu.png";
+	public static final String SOUND_BUTTONS = "sound_button.png";
+	public static final String URM_BUTTONS = "urm_buttons.png";
+	public static final String VOLUME_BUTTONS = "volume_buttons.png";
+	public static final String MENU_BACKGROUND_IMG = "background_menu.png";
+	public static final String PLAYING_BG_IMG = "playing_bg_img.png";
+	public static final String BIG_CLOUDS = "big_clouds.png";
+	public static final String SMALL_CLOUDS = "small_clouds.png";
+	public static final String CRABBY_SPRITE = "crabby_sprite.png";
+	public static final String STATUS_BAR = "health_power_bar.png";
+	public static final String COMPLETED_IMG = "completed_sprite.png";
+	public static final String POTION_ATLAS = "potions_sprites.png";
+	public static final String CONTAINER_ATLAS = "objects_sprites.png";
+	public static final String TRAP_ATLAS = "trap_atlas.png";
+	public static final String CANNON_ATLAS = "cannon_atlas.png";
+	public static final String CANNON_BALL = "ball.png";
+	public static final String DEATH_SCREEN = "death_screen.png";
+	public static final String OPTIONS_MENU = "options_background.png";
+	
+
+	public static BufferedImage GetSpriteAtlas(String fileName) {
+		BufferedImage img = null;
+		InputStream is = LoadSave.class.getResourceAsStream("/" + fileName);
+		try {
+			img = ImageIO.read(is);
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				is.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		return img;
+	}
+
+	
+	public static BufferedImage[] GetAllLevels() {
+
+	        List<String> levelNames = Arrays.asList("1.png", "2.png", "3.png");
+
+	        BufferedImage[] levels = new BufferedImage[levelNames.size()];
+
+	        for(int i = 0; i < levels.length; i++) {
+
+	            try {
+	                levels[i] = ImageIO.read(LoadSave.class.getResourceAsStream("/lvls/" + levelNames.get(i)));
+	            } catch (IOException e) {
+	                throw new RuntimeException(e);
+	            }
+	        }
+
+	        return levels;
+	    
+		
+//		URL url = LoadSave.class.getResource("/lvls");
+//		File file = null;
+//		
+//		try {
+//			file = new File(url.toURI());
+//		} catch (URISyntaxException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//		
+//		File[] files = file.listFiles();
+//		File[] filesSorted = new File[files.length];
+//		
+//		for(int i=0; i<filesSorted.length; i++)
+//			for( int j=0; j<files.length; j++) {
+//				if(files[j].getName().equals((i + 1) + ".png"))
+//					filesSorted[i] = files[j];
+//					
+//			}
+//		
+//		BufferedImage[] imgs = new BufferedImage[filesSorted.length];
+//		
+//		for(int i=0; i<imgs.length; i++)
+//			try {
+//				imgs[i] = ImageIO.read(filesSorted[i]);
+//			} catch (IOException e) {
+//				// TODO Auto-generated catch block
+//				e.printStackTrace();
+//			}
+//		
+//		return imgs;
+//		
+	}
+	
+	
+}
